b0565dfa84bc1c44f9656c86089070c8052ac6de
manavkandoi <mkandoi2003@gmail.com>
2023-06-02 22:35:48 +0530

    python part

--- assigns/01/MySolution/Python/assign01_06.py
+++ assigns/01/MySolution/Python/assign01_06.py
@@ -10,3 +10,28 @@ print("[import ./../../assign01_lib.py] is done!")
 # mylist_quicksort (see list_quicksort in assign01-lib.sml)
 #
 ####################################################
+
+
+def list_quicksort(xs):
+    def qsort(xs):
+        if not xs:
+            return []
+        else:
+            x1 = xs[0]
+            ys, zs = qpart(xs[1:], x1)
+            ys = qsort(ys)
+            zs = qsort(zs)
+            return ys + [x1] + zs
+
+    def qpart(xs, p0):
+        if not xs:
+            return [], []
+        else:
+            x1 = xs[0]
+            ys, zs = qpart(xs[1:], p0)
+            if x1 <= p0:
+                return [x1] + ys, zs
+            else:
+                return ys, [x1] + zs
+
+    return qsort(xs)
--- assigns/01/MySolution/Python/assign01_07.py
+++ assigns/01/MySolution/Python/assign01_07.py
@@ -10,3 +10,40 @@ print("[import ./../../assign01_lib.py] is done!")
 # mylist_mergesort (see list_mergesort in assign01-lib.sml)
 #
 ####################################################
+
+def mylist_mergesort(xs):
+    def split(xs):
+        if not xs:
+            return [], []
+        else:
+            x1 = xs[0]
+            if len(xs) == 1:
+                return [x1], []
+            else:
+                x2 = xs[1]
+                ys, zs = split(xs[2:])
+                return [x1] + ys, [x2] + zs
+
+    def merge(ys, zs):
+        if not ys:
+            return zs
+        elif not zs:
+            return ys
+        else:
+            y1 = ys[0]
+            z1 = zs[0]
+            if y1 <= z1:
+                return [y1] + merge(ys[1:], zs)
+            else:
+                return [z1] + merge(ys, zs[1:])
+
+    if not xs:
+        return []
+    else:
+        x1 = xs[0]
+        if len(xs) == 1:
+            return [x1]
+        else:
+            x2 = xs[1]
+            ys, zs = split(xs[2:])
+            return merge(mylist_mergesort([x1] + ys), mylist_mergesort([x2] + zs))
